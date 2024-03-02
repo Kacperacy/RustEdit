9d6ab774e930dc202c66cd4c4d3b16bdf9a0c4fa
Kacper Maciołek <kacper.maciolek00@gmail.com>
2024-03-02 11:28:43 +0100

    events added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use crossterm::{
+    event::{read, Event::Key, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let _ = enable_raw_mode();
+    loop {
+        if let Ok(Key(key)) = read() {
+            if key.code == KeyCode::Char('q') {
+                break;
+            } else {
+                println!("{:?}\r", key);
+            }
+        }
+    }
+    let _ = disable_raw_mode();
 }
