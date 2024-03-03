0e40aac961c23283b9a8b7b5cab02de742d44515
Kacper Maciołek <kacper.maciolek00@gmail.com>
2024-03-03 09:45:52 +0100

    poll added

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
+use std::time::Duration;
+
 use crossterm::{
-    event::{read, Event::Key, KeyCode},
+    event::{poll, read, Event::Key, KeyCode, KeyEvent},
     terminal::{disable_raw_mode, enable_raw_mode},
 };
 
 fn main() {
     let _ = enable_raw_mode();
     loop {
-        if let Ok(Key(key)) = read() {
-            if key.code == KeyCode::Char('q') {
-                break;
-            } else {
-                println!("{:?}\r", key);
+        let mut c: Option<KeyEvent> = None;
+
+        if let Ok(true) = poll(Duration::from_millis(100)) {
+            if let Ok(Key(key)) = read() {
+                c = Some(key);
             }
+        }
+
+        if let Some(c) = c {
+            println!("{c:?}\r");
+            if c.code == KeyCode::Char('q') {
+                break;
+            }
+        } else {
+            println!("No input\r");
         }
     }
     let _ = disable_raw_mode();
