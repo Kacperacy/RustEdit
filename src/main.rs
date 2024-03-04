f5e079cd0b93f841dedb1c54c0084cd24f99d13e
Kacper Maciołek <kacper.maciolek00@gmail.com>
2024-03-04 13:31:15 +0100

    functions added

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,11 @@
 use crossterm::{
-    event::{poll, read, Event::Key, KeyCode, KeyEvent},
+    event::{read, Event::Key, KeyCode, KeyEvent, KeyModifiers},
     terminal::{disable_raw_mode, enable_raw_mode},
 };
-use std::time::Duration;
 
 fn main() {
     let _ = enable_raw_mode();
-    loop {
-        let mut c: Option<KeyEvent> = None;
-
-        match poll(Duration::from_millis(100)) {
-            Ok(true) => {
-                if let Ok(Key(key)) = read() {
-                    c = Some(key);
-                } else {
-                    die("Read error")
-                }
-            }
-            Ok(false) => (),
-            Err(_) => die("Poll error"),
-        }
-
-        if let Some(c) = c {
-            println!("{c:?}\r");
-            if c.code == KeyCode::Char('q') {
-                break;
-            }
-        }
-    }
+    while editor_process_keypress() {}
     let _ = disable_raw_mode();
 }
 
@@ -35,4 +13,23 @@ fn die<S: Into<String>>(message: S) {
     let _ = disable_raw_mode();
     eprintln!("{}: {}", message.into(), std::io::Error::last_os_error());
     std::process::exit(1);
+}
+
+fn editor_read_key() -> Result<KeyEvent, ()> {
+    if let Ok(Key(key)) = read() {
+        Ok(key)
+    } else {
+        die("Read error");
+        Err(())
+    }
+}
+
+fn editor_process_keypress() -> bool {
+    match editor_read_key() {
+        Ok(c) => {
+            println!("{c:?}\r");
+            !(c.code == KeyCode::Char('q') && KeyModifiers::CONTROL == c.modifiers)
+        }
+        Err(_) => false,
+    }
 }
