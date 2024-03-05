ad67acec6e27773b1dca62ab0265454d4370e8dc
Kacper Maciołek <kacper.maciolek00@gmail.com>
2024-03-05 10:40:38 +0100

    editor refresh screen added

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,41 @@
 use crossterm::{
+    cursor::MoveTo,
     event::{read, Event::Key, KeyCode, KeyEvent, KeyModifiers},
-    terminal::{disable_raw_mode, enable_raw_mode},
+    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
 };
 
 fn main() {
     let _ = enable_raw_mode();
-    while editor_process_keypress() {}
+
+    editor_refresh_screen();
+    loop {
+        editor_refresh_screen();
+        if !editor_process_keypress() {
+            break;
+        }
+    }
+    editor_refresh_screen();
+
     let _ = disable_raw_mode();
 }
 
 fn die<S: Into<String>>(message: S) {
     let _ = disable_raw_mode();
+    editor_refresh_screen();
     eprintln!("{}: {}", message.into(), std::io::Error::last_os_error());
     std::process::exit(1);
+}
+
+fn editor_refresh_screen() {
+    print!("{} {}", Clear(ClearType::All), MoveTo(0, 0));
+    editor_draw_rows();
+    print!("{}", MoveTo(0, 0));
+}
+
+fn editor_draw_rows() {
+    for _ in 0..24 {
+        println!("~\r");
+    }
 }
 
 fn editor_read_key() -> Result<KeyEvent, ()> {
