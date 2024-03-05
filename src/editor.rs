b758e55f440336900c57f7c9484a8a184ea7e72a
Kacper Maciołek <kacper.maciolek00@gmail.com>
2024-03-05 11:03:50 +0100

    editor added

---
+++ src/editor.rs
@@ -0,0 +1,58 @@
+use crossterm::{
+    cursor::MoveTo,
+    event::{read, Event::Key, KeyCode, KeyEvent, KeyModifiers},
+    terminal::{disable_raw_mode, Clear, ClearType},
+};
+
+pub struct Editor {
+    screen_rows: usize,
+    screen_cols: usize,
+}
+
+impl Editor {
+    pub fn new() -> Self {
+        let (screen_cols, screen_rows) = crossterm::terminal::size().unwrap();
+        Self {
+            screen_rows: screen_rows as usize,
+            screen_cols: screen_cols as usize,
+        }
+    }
+
+    pub fn refresh_screen(&self) {
+        print!("{} {}", Clear(ClearType::All), MoveTo(0, 0));
+        self.draw_rows();
+        print!("{}", MoveTo(0, 0));
+    }
+
+    pub fn draw_rows(&self) {
+        for _ in 0..self.screen_rows {
+            println!("~\r");
+        }
+    }
+
+    pub fn read_key(&self) -> Result<KeyEvent, ()> {
+        if let Ok(Key(key)) = read() {
+            Ok(key)
+        } else {
+            self.die("Read error");
+            Err(())
+        }
+    }
+
+    pub fn process_keypress(&self) -> bool {
+        match self.read_key() {
+            Ok(c) => {
+                println!("{c:?}\r");
+                !(c.code == KeyCode::Char('q') && KeyModifiers::CONTROL == c.modifiers)
+            }
+            Err(_) => false,
+        }
+    }
+
+    pub fn die<S: Into<String>>(&self, message: S) {
+        let _ = disable_raw_mode();
+        self.refresh_screen();
+        eprintln!("{}: {}", message.into(), std::io::Error::last_os_error());
+        std::process::exit(1);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,20 @@
-use crossterm::{
-    cursor::MoveTo,
-    event::{read, Event::Key, KeyCode, KeyEvent, KeyModifiers},
-    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
-};
+mod editor;
+use editor::*;
+
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
 fn main() {
     let _ = enable_raw_mode();
+    let editor = Editor::new();
 
-    editor_refresh_screen();
+    editor.refresh_screen();
     loop {
-        editor_refresh_screen();
-        if !editor_process_keypress() {
+        editor.refresh_screen();
+        if !editor.process_keypress() {
             break;
         }
     }
-    editor_refresh_screen();
+    editor.refresh_screen();
 
     let _ = disable_raw_mode();
-}
-
-fn die<S: Into<String>>(message: S) {
-    let _ = disable_raw_mode();
-    editor_refresh_screen();
-    eprintln!("{}: {}", message.into(), std::io::Error::last_os_error());
-    std::process::exit(1);
-}
-
-fn editor_refresh_screen() {
-    print!("{} {}", Clear(ClearType::All), MoveTo(0, 0));
-    editor_draw_rows();
-    print!("{}", MoveTo(0, 0));
-}
-
-fn editor_draw_rows() {
-    for _ in 0..24 {
-        println!("~\r");
-    }
-}
-
-fn editor_read_key() -> Result<KeyEvent, ()> {
-    if let Ok(Key(key)) = read() {
-        Ok(key)
-    } else {
-        die("Read error");
-        Err(())
-    }
-}
-
-fn editor_process_keypress() -> bool {
-    match editor_read_key() {
-        Ok(c) => {
-            println!("{c:?}\r");
-            !(c.code == KeyCode::Char('q') && KeyModifiers::CONTROL == c.modifiers)
-        }
-        Err(_) => false,
-    }
 }
