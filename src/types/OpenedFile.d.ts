fb654cfe739d962809fad686a7047b83a67b781b
Kacper Maciołek <kacper.maciolek00@gmail.com>
2024-04-15 20:50:36 +0200

    cleanup

--- src/types/OpenedFile.d.ts
+++
@@ -1,9 +0,0 @@
-export default interface OpenedFile {
-  name: string;
-  filePath: string;
-  active: boolean;
-  //   lastModified: number;
-  //   webkitRelativePath: string;
-  //   size: number;
-  //   type: string;
-}
