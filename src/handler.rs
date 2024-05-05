1bf624d17c11ca226232597ce3d0d20fcea03368
Kacper Maciołek <kacper.maciolek00@gmail.com>
2024-05-05 21:16:14 +0200

    quit shortcut changes

--- src/handler.rs
+++ src/handler.rs
@@ -3,9 +3,6 @@ use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 
 pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
     match key_event.code {
-        KeyCode::Esc | KeyCode::Char('q') => {
-            app.quit();
-        }
         KeyCode::Char('c') | KeyCode::Char('C') => {
             if key_event.modifiers == KeyModifiers::CONTROL {
                 app.quit();
