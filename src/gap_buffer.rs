f9dfd8e85b0c02e3c04ab8775c2639914c55f2c6
Kacper Maciołek <kacper.maciolek00@gmail.com>
2024-06-10 23:30:41 +0200

    gap buffer p1

---
+++ src/gap_buffer.rs
@@ -0,0 +1,45 @@
+struct GapBuffer {
+    buffer: Vec<char>,
+    gap_start: usize,
+    gap_end: usize,
+}
+
+impl GapBuffer {
+    fn new(capacity: usize) -> GapBuffer {
+        GapBuffer {
+            buffer: vec![' '; capacity],
+            gap_start: 0,
+            gap_end: capacity,
+        }
+    }
+
+    fn insert(&mut self, c: char) {
+        if self.gap_start == self.gap_end {
+            self.resize();
+        }
+        self.buffer[self.gap_start] = c;
+        self.gap_start += 1;
+    }
+
+    fn resize(&mut self) {
+        let new_capacity = self.buffer.len() * 2;
+        let mut new_buffer = vec![' '; new_capacity];
+        let gap_size = self.gap_end - self.gap_start;
+
+        new_buffer[..self.gap_start].copy_from_slice(&self.buffer[..self.gap_start]);
+        new_buffer[new_capacity - gap_size..].copy_from_slice(&self.buffer[self.gap_end..]);
+
+        self.gap_end = new_capacity - gap_size;
+        self.buffer = new_buffer;
+    }
+
+    fn delete(&mut self) {
+        if self.gap_start > 0 {
+            self.gap_start -= 1;
+        }
+    }
+
+    fn move_cursor(&mut self, index: usize) {
+        if index < self.gap_start {}
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,5 @@ pub mod ui;
 pub mod tui;
 
 pub mod handler;
+
+pub mod gap_buffer;
